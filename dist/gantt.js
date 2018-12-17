2bac99b2e61f02a61c025d18c9964058555c54cb
helloyou2012 <helloyou2012@yeah.net>
2018-12-17 14:15:45 +0800

    dist build

--- dist/gantt.js
+++ dist/gantt.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.Gantt = {})));
-}(this, (function (exports) { 'use strict';
+	factory(global.Gantt = {});
+}(typeof self !== 'undefined' ? self : this, function (exports) { 'use strict';
 
 	function createCommonjsModule(fn, module) {
 		return module = { exports: {} }, fn(module, module.exports), module.exports;
@@ -1803,4 +1803,4 @@
 
 	Object.defineProperty(exports, '__esModule', { value: true });
 
-})));
+}));
